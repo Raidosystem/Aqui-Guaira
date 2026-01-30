9e0fa649e83b7b47ffcb8ccfd1eaf5fc3260b387
Grupo RaVal <gruporaval@MacBook-Pro-de-Grupo.local>
2026-01-30 13:16:05 -0300

    fix: remove extra closing bracket in AchadosPerdidos JSX

--- src/pages/AchadosPerdidos.tsx
+++ src/pages/AchadosPerdidos.tsx
@@ -380,7 +380,6 @@ export default function AchadosPerdidos() {
                         </Button>
                       </DialogFooter>
                     </form>
-                  )}
                 </DialogContent>
               </Dialog>
             </div>
